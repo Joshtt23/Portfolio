630da91b9b2e1944eede8a2e5b1ad4af562e38d5
The Lost Toastr <josht98@aol.com>
2023-01-03 21:08:12 -0500

    added firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider  } from "react-router-dom";
 import AllRoutes from "./routes";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBCXyoNBIecE2E1LzZE37jzHlaM-8MBZaE",
+  authDomain: "joshua-travers-portfolio.firebaseapp.com",
+  projectId: "joshua-travers-portfolio",
+  storageBucket: "joshua-travers-portfolio.appspot.com",
+  messagingSenderId: "997220489043",
+  appId: "1:997220489043:web:19be0f68492315c973f3a6",
+  measurementId: "G-1VLF2LMRQF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,27 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Projects.css";
 
 function Projects() {
+
+  // get each category from database and map with img name and descriptions
+
+
+  // const [ liveProjects, setLiveProjects ] = useState({})
+  // const [ experienceProjects, setExperienceProjects ] = useState({})
+
+  // liveProjects.map(project => {
+  //   <div className="project-card">
+  //     <img src={project.img} alt="Project Image"/>
+  //     <a href={project.link} alt="Project Link">{project.name}</a>
+  //     <p>{project.description}</p>
+  //   </div>
+  // })
+  
+  // experienceProjects.map(project => {
+  //   <div className="project-card">
+  //     <img src={project.img} alt="Project Image"/>
+  //     <a href={project.link} alt="Project Link">{project.name}</a>
+  //     <p>{project.description}</p>
+  //   </div>
+  // })
+
+  // useEffect(() => {
+    
+  
+    
+  // }, [])
+  
+
   return (
     <div className="all-projects">
-      <div className="yrobot">
-        <img src="./images/yRobot-preview.png" alt="yRobot Preview" />
-        <br/>
-        <a href="https://yrobot.us">ŷRobot</a>
-        <p>
-          Artificial intelligence based machine learning crypto currency signal
-          service with additional analysis such as hype and full forward term
-          regression
-        </p>
+      <div className="live-projects">
+        <h2>Live Projects</h2>
+        <div className="yrobot">
+          <img src="./images/yRobot-preview.png" alt="yRobot Preview" />
+          <br />
+          <a href="https://yrobot.us">ŷRobot</a>
+          <p>
+            Artificial intelligence based machine learning crypto currency
+            signal service with additional analysis such as hype and full
+            forward term regression
+          </p>
+        </div>
+        <div className="toasty-friends">
+          <img src="./images/tf-preview.png" alt="Toasty Friends Preview" />
+          <br />
+          <a href="https://toastyfriends.club"> Toasty Friends</a>
+          <p>
+            Non-Fungible Token project to convert API data to quickly accessible
+            and useable trading data for precise and profitable trades.
+          </p>
+        </div>
       </div>
-      <div className="toasty-friends">
-        <img src="./images/tf-preview.png" alt="Toasty Friends Preview" />
-        <br/>
-        <a href="https://toastyfriends.club"> Toasty Friends</a>
-        <p>
-          Non-Fungible Token project to convert API data to quickly accessible
-          and useable trading data for precise and profitable trades.
-        </p>
+      <div className="learning-projects">
+        <h2>Experience Projects</h2>
+
       </div>
     </div>
   );

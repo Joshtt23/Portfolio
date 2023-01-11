de5ac7841e302d17c1e18fea1e1ef24001aec3e7
The Lost Toastr <josht98@aol.com>
2023-01-11 02:55:50 -0500

    minor styling and begin rework of contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
-import "./styles/App.css"
-
+import "./styles/App.css";
+import { initializeApp } from "firebase/app"
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBCXyoNBIecE2E1LzZE37jzHlaM-8MBZaE",
+    authDomain: "joshua-travers-portfolio.firebaseapp.com",
+    projectId: "joshua-travers-portfolio",
+    storageBucket: "joshua-travers-portfolio.appspot.com",
+    messagingSenderId: "997220489043",
+    appId: "1:997220489043:web:19be0f68492315c973f3a6",
+    measurementId: "G-1VLF2LMRQF",
+  };
+
+  const app = initializeApp(firebaseConfig)
+  
+
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <div className="page-content">
-        <Outlet/>
+        <Outlet />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,36 +1,39 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Projects.css";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 
 function Projects() {
+  const [liveProjects, setLiveProjects] = useState({});
+  const [experienceProjects, setExperienceProjects] = useState({});
+  const [loadProjects, setLoadProjects] = useState(false);
 
-  // get each category from database and map with img name and descriptions
+  async function getProjects() {
+    const db = getFirestore();
 
+    const liveProjects = await getDocs(collection(db, "LiveProjects"));
+    const experienceProjects = await getDocs(
+      collection(db, "ExperienceProjects")
+    );
 
-  // const [ liveProjects, setLiveProjects ] = useState({})
-  // const [ experienceProjects, setExperienceProjects ] = useState({})
+    setExperienceProjects(experienceProjects);
+    setLoadProjects(true);
+    setLiveProjects(liveProjects);
+  }
 
-  // liveProjects.map(project => {
-  //   <div className="project-card">
-  //     <img src={project.img} alt="Project Image"/>
-  //     <a href={project.link} alt="Project Link">{project.name}</a>
-  //     <p>{project.description}</p>
-  //   </div>
-  // })
-  
-  // experienceProjects.map(project => {
-  //   <div className="project-card">
-  //     <img src={project.img} alt="Project Image"/>
-  //     <a href={project.link} alt="Project Link">{project.name}</a>
-  //     <p>{project.description}</p>
-  //   </div>
-  // })
+  useEffect(() => {
+    const onPageLoad = () => {
+      getProjects();
+    };
 
-  // useEffect(() => {
-    
-  
-    
-  // }, [])
-  
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
 
   return (
     <div className="all-projects">
@@ -41,9 +44,9 @@ function Projects() {
           <br />
           <a href="https://yrobot.us">ŷRobot</a>
           <p>
-            Artificial intelligence based machine learning crypto currency
-            signal service with additional analysis such as hype and full
-            forward term regression
+            Machine learning based artificial intelligence to provide crypto currency
+            signal services, with additional analysis such as sentiment and full
+            term regression.
           </p>
         </div>
         <div className="toasty-friends">
@@ -51,15 +54,49 @@ function Projects() {
           <br />
           <a href="https://toastyfriends.club"> Toasty Friends</a>
           <p>
-            Non-Fungible Token project to convert API data to quickly accessible
-            and useable trading data for precise and profitable trades.
+            Non-Fungible Token project to convert marketplace API data to quickly accessible
+            and easy to read trading data for precise and profitable trades in the markets.
           </p>
         </div>
       </div>
-      <div className="learning-projects">
+      {/* <div className="learning-projects">
         <h2>Experience Projects</h2>
 
-      </div>
+      </div> */}
+      {1==2 ? (
+        <>
+          <div className="live-projects">
+            <h2>Live Projects</h2>
+            {liveProjects.forEach((project) => {
+              return (
+                <div className="project-card">
+                  <img src={project.data().Img} alt="Project Image" />
+                  <a href={project.data().Link} alt="Project Link">
+                    {project.data().Name}
+                  </a>
+                  <p>{project.data().Description}</p>
+                </div>
+              );
+            })}
+          </div>
+          <div className="learning-projects">
+            <h2>Experimental Projects</h2>
+            {experienceProjects.forEach((project) => {
+              return (
+                <div className="project-card">
+                  <img src={project.data().img} alt="Project Image" />
+                  <a href={project.data().link} alt="Project Link">
+                    {project.data().name}
+                  </a>
+                  <p>{project.data().description}</p>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
